11ff8f76f9bc673df0a21982e30f82bdb84282ff
Thilina-Shamika <thilinashamika@gmail.com>
2025-12-24 18:35:54 +0530

    google tags for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,22 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NKKNF587');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Google tag (gtag.js) sending again */}
         <Script
           id="gtag-base"
@@ -116,6 +132,15 @@ export default async function RootLayout({
       <body
         className={`${poppins.variable} font-sans antialiased bg-black`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKKNF587"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
+
         {/* Meta Pixel noscript fallback */}
         <noscript>
           <img
